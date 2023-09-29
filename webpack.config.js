6e710a03eae7d70e890d54c89d2e06991a7963e8
maryia-matskevich <136838609+maryia-matskevich@users.noreply.github.com>
2023-09-29 12:59:51 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'eval-source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
+            }
+        ],
+    },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css",
+            }),
+        ],
+};
