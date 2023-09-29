bcd402eda7fdbd86285bb8450a370f5d68fae6dc
maryia-matskevich <136838609+maryia-matskevich@users.noreply.github.com>
2023-09-29 12:56:18 +0300

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,126 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _someModule__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./someModule */ \"./src/someModule.js\");\n/* harmony import */ var _styles__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles */ \"./src/styles/index.js\");\n\n\nconst instance = new _someModule__WEBPACK_IMPORTED_MODULE_0__.SomeModule();\ninstance.test();//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7O0FBQTBDO0FBQ3hCO0FBRWxCLE1BQU1DLFFBQVEsR0FBRyxJQUFJRCxtREFBVSxDQUFDLENBQUM7QUFDakNDLFFBQVEsQ0FBQ0MsSUFBSSxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZXN0LXdlYnBhY2svLi9zcmMvaW5kZXguanM/YjYzNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBTb21lTW9kdWxlIH0gZnJvbSAnLi9zb21lTW9kdWxlJztcbmltcG9ydCAnLi9zdHlsZXMnO1xuXG5jb25zdCBpbnN0YW5jZSA9IG5ldyBTb21lTW9kdWxlKCk7XG5pbnN0YW5jZS50ZXN0KCk7Il0sIm5hbWVzIjpbIlNvbWVNb2R1bGUiLCJpbnN0YW5jZSIsInRlc3QiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ }),
+
+/***/ "./src/someModule.js":
+/*!***************************!*\
+  !*** ./src/someModule.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   SomeModule: () => (/* binding */ SomeModule)\n/* harmony export */ });\nclass SomeModule {\n  test() {\n    console.log('TEST');\n  }\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc29tZU1vZHVsZS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsTUFBTUEsVUFBVSxDQUFDO0VBQ2ZDLElBQUlBLENBQUEsRUFBRztJQUNMQyxPQUFPLENBQUNDLEdBQUcsQ0FBQyxNQUFNLENBQUM7RUFDckI7QUFDRiIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3Qtd2VicGFjay8uL3NyYy9zb21lTW9kdWxlLmpzP2E0MWQiXSwic291cmNlc0NvbnRlbnQiOlsiY2xhc3MgU29tZU1vZHVsZSB7XG4gIHRlc3QoKSB7XG4gICAgY29uc29sZS5sb2coJ1RFU1QnKTtcbiAgfVxufVxuXG5leHBvcnQgeyBTb21lTW9kdWxlIH07Il0sIm5hbWVzIjpbIlNvbWVNb2R1bGUiLCJ0ZXN0IiwiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/someModule.js\n");
+
+/***/ }),
+
+/***/ "./src/styles/index.js":
+/*!*****************************!*\
+  !*** ./src/styles/index.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test.css */ \"./src/styles/test.css\");\n/* harmony import */ var _otherTest_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./otherTest.scss */ \"./src/styles/otherTest.scss\");\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3R5bGVzL2luZGV4LmpzIiwibWFwcGluZ3MiOiI7OztBQUFvQiIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3Qtd2VicGFjay8uL3NyYy9zdHlsZXMvaW5kZXguanM/MjI3MiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgJy4vdGVzdC5jc3MnO1xuaW1wb3J0ICcuL290aGVyVGVzdC5zY3NzJzsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/styles/index.js\n");
+
+/***/ }),
+
+/***/ "./src/styles/otherTest.scss":
+/*!***********************************!*\
+  !*** ./src/styles/otherTest.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3R5bGVzL290aGVyVGVzdC5zY3NzIiwibWFwcGluZ3MiOiI7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3Qtd2VicGFjay8uL3NyYy9zdHlsZXMvb3RoZXJUZXN0LnNjc3M/N2IxMiJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/styles/otherTest.scss\n");
+
+/***/ }),
+
+/***/ "./src/styles/test.css":
+/*!*****************************!*\
+  !*** ./src/styles/test.css ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3R5bGVzL3Rlc3QuY3NzIiwibWFwcGluZ3MiOiI7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3Qtd2VicGFjay8uL3NyYy9zdHlsZXMvdGVzdC5jc3M/Zjg2MSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/styles/test.css\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
